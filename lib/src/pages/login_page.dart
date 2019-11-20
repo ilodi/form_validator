31c89703dc6fff9f1f7410d150ea42e3c189cf92
David Lodi <lodi.dev@outlook.com>
2019-11-19 22:37:03 -0600

    all disignert

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -6,9 +6,174 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login'),
+      body: Stack(
+        children: <Widget>[
+          _crearFondo(context),
+          _loginForm(context),
+          
+        ],
       ),
+    );
+  }
+
+  Widget _loginForm(BuildContext context) {
+    //OBTENER EL 40% DE la pantalla
+    final size = MediaQuery.of(context).size;
+    //Hace scroll depende del tamaño de su hijo
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SafeArea(
+            child: Container(
+              height: 180.0,
+            ),
+          ),
+          Container(
+            width: size.width * 0.85,
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            margin: EdgeInsets.symmetric(vertical: 30.0),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 3.0,
+                    spreadRadius: 3.0,
+                    offset: Offset(0.0, 5.0),
+                  )
+                ]),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'INGRESO',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                SizedBox(height: 20.0),
+                _crearEmail(),
+                SizedBox(height: 30.0),
+                _crearPass(),
+                SizedBox(height: 30.0),
+                _creatBotn(),
+              ],
+            ),
+          ),
+          Text('¿Olvido la contreaseña?'),
+          SizedBox(height: 100.0,)
+        ],
+      ),
+    );
+  }
+
+  Widget _crearEmail() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            icon: Icon(Icons.alternate_email, color: Colors.cyan),
+            hintText: 'ejemplo@mail.com',
+            labelText: 'Correo Electronico'),
+      ),
+    );
+  }
+
+  Widget _creatBotn() {
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 20.0),
+        child: Text('Ingresar'),
+      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+      elevation: 0.0,
+      color:  Color.fromRGBO(0, 103, 185, 1.0),
+      textColor: Colors.white,
+      onPressed: () {},
+    );
+  }
+
+  Widget _crearPass() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        obscureText: true,
+        keyboardType: TextInputType.visiblePassword,
+        decoration: InputDecoration(
+            icon: Icon(Icons.vpn_key, color: Colors.cyan),
+            labelText: 'Password'),
+      ),
+    );
+  }
+
+  Widget _crearFondo(BuildContext context) {
+    //OBTENER EL 40% DE la pantalla
+    final size = MediaQuery.of(context).size;
+    final fondoMorado = Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: <Color>[
+        Color.fromRGBO(0, 103, 185, 1.0),
+        Color.fromRGBO(0, 88, 161, 1.0),
+      ])),
+    );
+
+    ///
+    final circulo = Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Color.fromRGBO(255, 255, 255, 0.05)),
+    );
+
+    return Stack(
+      children: <Widget>[
+        fondoMorado,
+        Positioned(
+          child: circulo,
+          top: 90.0,
+          left: 30.0,
+        ),
+        Positioned(
+          child: circulo,
+          top: -40.0,
+          right: -30.0,
+        ),
+        Positioned(
+          child: circulo,
+          bottom: -50.0,
+          right: 50.0,
+        ),
+        Positioned(
+          child: circulo,
+          bottom: 120.0,
+          right: 20.0,
+        ),
+        Positioned(
+          child: circulo,
+          bottom: -50.0,
+          left: -20.0,
+        ),
+        Container(
+          padding: EdgeInsets.only(top: 80.0),
+          child: Column(
+            children: <Widget>[
+              Icon(
+                Icons.person_pin_circle,
+                color: Colors.white,
+                size: 100.0,
+              ),
+              SizedBox(
+                height: 10.0,
+                width: double.infinity,
+              ),
+              Text('David E. Lopez',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0)),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
