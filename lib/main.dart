4a9bf81f4bda9219aecab84f99d869fc48460c77
David Lodi <lodi.dev@outlook.com>
2019-11-19 21:30:13 -0600

    base login styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
- 
+import 'package:formvalidator/src/pages/home_page.dart';
+import 'package:formvalidator/src/pages/login_page.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => LoginPage(),
+        'home': (BuildContext context) => HomePage()
+      },
     );
   }
 }
